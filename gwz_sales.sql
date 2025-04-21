e9e7b15e6b79911c8a4bd0651fe60e473569b705
Mai Middleton <mai-salama@live.com>
2025-04-21 15:32:32 +0100

    sort_dates

--- gwz_sales.sql
+++ gwz_sales.sql
@@ -1,12 +1,10 @@
--- gwz_sales.sql
-
 SELECT
   date_date AS sale_date,
-  SUM(turnover_before_promo) AS daily_turnover,
-  SUM(purchase_cost) AS daily_purchase_cost
+  ROUND(SUM(turnover_before_promo), 2) AS daily_turnover,
+  ROUND(SUM(purchase_cost), 2) AS daily_purchase_cost
 FROM
   `data-analytics-bootcamp-363212.course14.gwz_sales`
 GROUP BY
   sale_date
 ORDER BY
-  sale_date;
+  sale_date DESC;
